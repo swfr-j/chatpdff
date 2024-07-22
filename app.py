e5f007e462a772b82daf1d90eccfd6df527500b6
swfr-j <130271263+swfr-j@users.noreply.github.com>
2024-07-22 14:12:27 +0530

    Add pypdf2 raw text

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 from dotenv import load_dotenv
-import os
+from PyPDF2 import PdfReader
 
 load_dotenv()
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 
 def main():
@@ -20,7 +29,8 @@ def main():
         if st.button("Process"):
             with st.spinner("Processing your documents..."):
                 # get pdf text
-                pass
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
 
                 # get the text chunks
 
