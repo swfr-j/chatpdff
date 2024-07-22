a4d27c811922989b6bfaab13fa2797d74d395b5c
swfr-j <130271263+swfr-j@users.noreply.github.com>
2024-07-22 14:16:58 +0530

    Add character text splitter for chunking text

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 load_dotenv()
 
@@ -12,6 +13,14 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 
 def main():
@@ -30,11 +39,14 @@ def main():
             with st.spinner("Processing your documents..."):
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-                st.write(raw_text)
 
+            with st.spinner("Splitting text into chunks..."):
                 # get the text chunks
+                chunks = get_text_chunks(raw_text)
 
+            with st.spinner("Creating vector store..."):
                 # Create vector store
+                pass
 
 
 if __name__ == "__main__":
